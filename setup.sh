500c859bf84a0a175999a4a949b5d5603ef2a303
LearnEHKing <87217112+LearnEHKing@users.noreply.github.com>
2026-07-27 09:34:23 +0530

    Added setup.sh, requirments.txt. Updated .gitignore

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir Data
+touch Data/config.json
+touch Data/users.json
+echo '''{"TOKEN":"","OWNER_ID":""}''' > Data/config.json
+echo "[]">Data/users.json
+pythom -m pip install -r requirments.txt
